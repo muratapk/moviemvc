0fef0a52e360c9efc99e1aa8055c3835eaf0e324
muratapk <muratapk@hotmail.com>
2023-10-07 15:49:59 +0300

    Add project files.

---
+++ moviemvc/Data/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace moviemvc.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        protected DatabaseContext(DbContextOptions<DatabaseContext> options):base(options)
+        {
+        }
+
+    }
+}
---
+++ moviemvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace moviemvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
