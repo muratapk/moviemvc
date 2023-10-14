111f72f2ab4c348bcd42fbf79db2c8b782eb16a1
muratapk <muratapk@hotmail.com>
2023-10-14 16:28:12 +0300

    detais eklendi

--- moviemvc/Controllers/MoviesController.cs
+++ moviemvc/Controllers/MoviesController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage;
+using moviemvc.Data;
 
 namespace moviemvc.Controllers
 {
     public class MoviesController : Controller
     {
+        public readonly DatabaseContext _context;
+
+        public MoviesController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Details(int? id)
+        {
+            var tekveri=_context.Movies.Find(id);
+            return View(tekveri);
         }
     }
 }
