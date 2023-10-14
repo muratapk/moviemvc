c254e293ce7604232a0cd97e3c8370bd00499797
muratapk <muratapk@hotmail.com>
2023-10-14 15:16:05 +0300

    Movie Controller

--- moviemvc/ViewComponents/MovieList.cs
+++ moviemvc/ViewComponents/MovieList.cs
@@ -1,6 +1,20 @@
-﻿namespace moviemvc.ViewComponents
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
+using moviemvc.Data;
+
+namespace moviemvc.ViewComponents
 {
-    public class MovieList
+    public class MovieList:ViewComponent
     {
+        private readonly DatabaseContext _context;
+        public MovieList(DatabaseContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var liste = _context.Movies.ToList();
+            return View(liste);
+        }
     }
 }
